9c062ecb1e663e0f81605c492a5c47521c59c767
Rubel Mahmud <vxlrubel@gmail.com>
2026-01-16 01:49:41 +0600

    delete some existing icon and add a new icon called plus

---
+++ src/components/icons/IconPlus.vue
@@ -0,0 +1,11 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    height="16px"
+    viewBox="0 -960 960 960"
+    width="16px"
+    fill="currentColor"
+  >
+    <path d="M417-417H166v-126h251v-251h126v251h251v126H543v251H417v-251Z" />
+  </svg>
+</template>
