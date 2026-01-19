b206b2eaaf3cb79b61d0fd92b49e71dbf9ca462e
Rubel Mahmud <vxlrubel@gmail.com>
2026-01-20 03:31:22 +0600

    add login/register component with sign-out functionality

---
+++ src/components/LoginRegister.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { RouterLink, useRouter } from 'vue-router'
+import { useAuth } from '@/stores/auth'
+const auth = useAuth()
+const router = useRouter()
+
+const signOut = () => {
+  auth.logoutUser()
+  router.push('/login')
+}
+</script>
+
+<template>
+  <div class="font-bold flex items-center gap-3 justify-end">
+    <RouterLink to="/login">
+      <i class="pi pi-sign-in"></i>
+    </RouterLink>
+    <button
+      v-if="auth.isLoggedIn"
+      @click="signOut"
+      class="cursor-pointer text-red-600 hover:text-gray-50 transition-colors duration-300"
+    >
+      <i class="pi pi-power-off"></i>
+    </button>
+  </div>
+</template>
