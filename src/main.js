67873ea186ad69f06529a0d89a73a5e747c011fc
Rubel Mahmud <vxlrubel@gmail.com>
2026-01-20 02:30:05 +0600

    initialize authentication store and load user data from storage

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import 'primeicons/primeicons.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { useAuth } from '@/stores/auth'
 import PrimeVue from 'primevue/config'
 import Aura from '@primeuix/themes/aura'
 import ToastService from 'primevue/toastservice'
@@ -12,7 +13,6 @@ import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
-
 app.use(createPinia())
 app.use(router)
 app.use(PrimeVue, {
@@ -24,3 +24,6 @@ app.use(ToastService)
 app.use(ConfirmationService)
 
 app.mount('#app')
+
+const auth = useAuth()
+auth.loadFromStorage()
