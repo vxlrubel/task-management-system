20279095ff9d0dd27ff232cb612697268da3b647
Rubel Mahmud <vxlrubel@gmail.com>
2026-01-21 02:27:39 +0600

    refactor: update router configuration to use dynamic imports and add authentication checks

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import DashboardView from '../views/DashboardView.vue'
+import { useAuth } from '@/stores/auth'
+import DashboardView from '@/views/DashboardView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,58 +9,84 @@ const router = createRouter({
       path: '/',
       name: 'Dashboard',
       component: DashboardView,
+      meta: { requiresAuth: true },
     },
     {
       path: '/my-tasks',
       name: 'My Tasks',
-      component: () => import('../views/MytasksView.vue'),
+      component: () => import('@/views/MytasksView.vue'),
+      meta: { requiresAuth: true },
     },
     {
       path: '/users',
       name: 'Users',
-      component: () => import('../views/UsersView.vue'),
-    },
-    {
-      path: '/user/:id',
-      name: 'Update User Info',
-      component: () => import('../views/EditUserView.vue'),
+      component: () => import('@/views/UsersView.vue'),
+      meta: { requiresAuth: true },
     },
     {
       path: '/user/add-new',
-      name: 'Add new user',
-      component: () => import('../views/AddUserView.vue'),
+      name: 'Add New User',
+      component: () => import('@/views/AddUserView.vue'),
+      meta: { requiresAuth: true },
+    },
+    {
+      path: '/user/:id',
+      name: 'Edit User',
+      component: () => import('@/views/EditUserView.vue'),
+      meta: { requiresAuth: true },
     },
     {
       path: '/projects',
       name: 'Projects',
-      component: () => import('../views/ProjectsView.vue'),
+      component: () => import('@/views/ProjectsView.vue'),
+      meta: { requiresAuth: true },
     },
     {
       path: '/project/add-new',
-      name: 'Add new project',
-      component: () => import('../views/AddProjectView.vue'),
+      name: 'Add Project',
+      component: () => import('@/views/AddProjectView.vue'),
+      meta: { requiresAuth: true },
     },
     {
       path: '/teams',
       name: 'Teams',
-      component: () => import('../views/TeamsView.vue'),
-    },
-    {
-      path: '/team/:id',
-      name: 'Update Team Info',
-      component: () => import('../views/EditTeamView.vue'),
+      component: () => import('@/views/TeamsView.vue'),
+      meta: { requiresAuth: true },
     },
     {
       path: '/team/add-new',
-      name: 'Add new team',
-      component: () => import('../views/AddTeamView.vue'),
+      name: 'Add Team',
+      component: () => import('@/views/AddTeamView.vue'),
+      meta: { requiresAuth: true },
+    },
+    {
+      path: '/team/:id',
+      name: 'Edit Team',
+      component: () => import('@/views/EditTeamView.vue'),
+      meta: { requiresAuth: true },
     },
     {
       path: '/login',
       name: 'Login',
-      component: () => import('../views/LoginView.vue'),
+      component: () => import('@/views/LoginView.vue'),
     },
   ],
+})
+
+router.beforeEach((to, from, next) => {
+  const auth = useAuth()
+
+  if (to.meta.requiresAuth && !auth.isLoggedIn) {
+    next({ name: 'Login' })
+    return
+  }
+
+  if (to.name === 'Login' && auth.isLoggedIn) {
+    next({ name: 'Dashboard' })
+    return
+  }
+
+  next()
 })
 
 export default router
