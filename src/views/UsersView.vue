ab0e5917a9b7ef8e03fa383ab0851ae3ef51e524
Rubel Mahmud <vxlrubel@gmail.com>
2026-01-23 02:37:50 +0600

    Optimize: distruct the function  for delete and add the show link

--- src/views/UsersView.vue
+++ src/views/UsersView.vue
@@ -1,8 +1,8 @@
 <script setup>
 import { onMounted } from 'vue'
 import { storeToRefs } from 'pinia'
+import { useRouter } from 'vue-router'
 import { useUserStore } from '@/stores/user'
-import { RouterLink } from 'vue-router'
 import Toast from 'primevue/toast'
 import ConfirmPopup from 'primevue/confirmpopup'
 import { useConfirm } from 'primevue/useconfirm'
@@ -11,6 +11,7 @@ const confirm = useConfirm()
 const toast = useToast()
 const userStore = useUserStore()
 const { users, loading, error, userCount, hasUsers } = storeToRefs(userStore)
+const router = useRouter()
 onMounted(() => {
   userStore.fetchUsers()
 })
@@ -37,6 +38,14 @@ const delUser = (id) => {
     },
   })
 }
+
+const visitDetails = (id) => {
+  router.push(`/user/show/${id}`)
+}
+
+const visitEdit = (id) => {
+  router.push(`/user/${id}`)
+}
 </script>
 <template>
   <div class="p-4">
@@ -61,27 +70,34 @@ const delUser = (id) => {
     <!-- Users list -->
     <div v-else-if="hasUsers" class="space-y-2">
       <p class="text-sm mb-2">Total users: {{ userCount }}</p>
-      <div v-for="user in users" :key="user.id" class="p-4 border border-gray-300 rounded">
-        <div class="flex justify-between">
-          <div class="font-medium">{{ user.name }}</div>
-          <div>
-            <RouterLink
-              :to="'/user/' + user.id"
-              type="button"
-              class="bg-blue-500 text-sm inline-flex items-center justify-center h-8 w-8 rounded-full cursor-pointer hover:bg-blue-600 me-3"
-            >
-              <i class="pi pi-pen-to-square text-sm"></i>
-            </RouterLink>
-            <button
-              @click="delUser(user.id)"
-              type="button"
-              class="bg-red-500 text-sm inline-flex items-center justify-center h-8 w-8 rounded-full cursor-pointer hover:bg-red-600"
-            >
-              <i class="pi pi-trash text-sm"></i>
-            </button>
+      <div class="border-b border-gray-600 border-l border-r">
+        <div
+          v-for="user in users"
+          :key="user.id"
+          @click="visitDetails(user.id)"
+          class="p-3 border-t border-gray-600 cursor-pointer hover:bg-[hsl(210,6%,15%)]"
+        >
+          <div class="flex justify-between">
+            <div class="font-medium">{{ user.name }}</div>
+            <div>
+              <button
+                @click.stop="visitEdit(user.id)"
+                type="button"
+                class="bg-blue-500 text-sm inline-flex items-center justify-center h-8 w-8 rounded-full cursor-pointer hover:bg-blue-600 me-3"
+              >
+                <i class="pi pi-pen-to-square text-sm"></i>
+              </button>
+              <button
+                @click.stop="delUser(user.id)"
+                type="button"
+                class="bg-red-500 text-sm inline-flex items-center justify-center h-8 w-8 rounded-full cursor-pointer hover:bg-red-600"
+              >
+                <i class="pi pi-trash text-sm"></i>
+              </button>
+            </div>
           </div>
+          <p class="text-sm text-gray-400">{{ user.email }}</p>
         </div>
-        <p class="text-sm text-gray-300">{{ user.email }}</p>
       </div>
     </div>
 
