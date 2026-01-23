9f804d1e4685a55083a18b7c5cbe8690c75476e5
Rubel Mahmud <vxlrubel@gmail.com>
2026-01-23 22:40:41 +0600

    create reusable items component for loop initiate

---
+++ src/components/ItemsView.vue
@@ -0,0 +1,46 @@
+<script setup>
+import { defineProps } from 'vue'
+defineProps({
+  items: {
+    type: Array,
+    required: true,
+  },
+})
+const itemInitials = (name) => {
+  if (!name) return ''
+  return name
+    .split(' ')
+    .map((n) => n[0].toUpperCase())
+    .slice(0, 2)
+    .join('')
+}
+</script>
+<template>
+  <div>
+    <div
+      class="flex mb-2 hover:bg-gray-600/20 cursor-pointer"
+      v-for="(item, index) in items"
+      :key="item.id + index"
+    >
+      <div
+        class="h-14 w-14 rounded-full bg-gray-600 flex items-center justify-center text-white text-[17px] font-bold"
+      >
+        {{ itemInitials(item.name) }}
+      </div>
+
+      <!-- Content -->
+      <div class="flex-1 pl-3">
+        <div class="font-medium">
+          {{ item.name }}
+          <span
+            v-if="item.status === 'active'"
+            class="inline-flex h-2 w-2 ml-1 rounded-full bg-green-400"
+          ></span>
+        </div>
+        <div class="text-sm text-gray-400">
+          {{ item.email }} <span v-if="item.team_role">| {{ item.team_role }}</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
