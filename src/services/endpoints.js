feabd85d6f9b404f1218563f754bb0b9aaac629f
Rubel Mahmud <vxlrubel@gmail.com>
2026-01-24 00:24:47 +0600

    Register other API routes

--- src/services/endpoints.js
+++ src/services/endpoints.js
@@ -21,3 +21,43 @@ export const memberEndpoint = {
   update: (id) => `/team_members/${id}`,
   delete: (id) => `/team_members/${id}`,
 }
+
+export const projectEndpoint = {
+  list: '/projects',
+  create: '/projects',
+  single: (id) => `/projects/${id}`,
+  update: (id) => `/projects/${id}`,
+  delete: (id) => `/projects/${id}`,
+}
+
+export const taskEndpoint = {
+  list: '/tasks',
+  create: '/tasks',
+  single: (id) => `/tasks/${id}`,
+  update: (id) => `/tasks/${id}`,
+  delete: (id) => `/tasks/${id}`,
+}
+
+export const commentEndpoint = {
+  list: '/task_comments',
+  create: '/task_comments',
+  single: (id) => `/task_comments/${id}`,
+  update: (id) => `/task_comments/${id}`,
+  delete: (id) => `/task_comments/${id}`,
+}
+
+export const logsEndpoint = {
+  list: '/task_activity_logs',
+  create: '/task_activity_logs',
+  single: (id) => `/task_activity_logs/${id}`,
+  update: (id) => `/task_activity_logs/${id}`,
+  delete: (id) => `/task_activity_logs/${id}`,
+}
+
+export const notificationEndpoint = {
+  list: '/notifications',
+  create: '/notifications',
+  single: (id) => `/notifications/${id}`,
+  update: (id) => `/notifications/${id}`,
+  delete: (id) => `/notifications/${id}`,
+}
