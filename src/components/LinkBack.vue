54b6db23b4c7b5624f68b7ce8e9bfde376863385
Rubel Mahmud <vxlrubel@gmail.com>
2026-01-23 20:46:50 +0600

    create backlink component for move backword

---
+++ src/components/LinkBack.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { useRouter } from 'vue-router'
+const router = useRouter()
+</script>
+
+<template>
+  <button
+    @click="router.back()"
+    class="w-13 h-7 items-center cursor-pointer justify-center bg-[hsl(210,6%,20%)] inline-flex text-sm hover:bg-[hsl(210,6%,25%)] transition-colors"
+  >
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      height="18px"
+      viewBox="0 -960 960 960"
+      width="18px"
+      fill="currentColor"
+    >
+      <path d="M360-240 120-480l240-240 56 56-144 144h568v80H272l144 144-56 56Z" />
+    </svg>
+  </button>
+</template>
