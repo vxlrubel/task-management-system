cbdc6e46ca0e42afd9a09a6e391a2c974b4ea17e
Rubel Mahmud <vxlrubel@gmail.com>
2026-01-27 02:22:34 +0600

    create a page for single project

---
+++ src/views/project/ShowView.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { onMounted } from 'vue'
+import { storeToRefs } from 'pinia'
+import { useRoute } from 'vue-router'
+import { useProjectStore } from '@/stores/project'
+
+const projectStore = useProjectStore()
+const { project } = storeToRefs(projectStore)
+const route = useRoute()
+
+onMounted(async () => {
+  projectStore.fetchProjectById(route.params.id)
+})
+</script>
+
+<template>
+  <div>
+    <pre>{{ project }}</pre>
+  </div>
+</template>
