573cef62e718ba8487fed2db29a0e29c5769c5b4
Rubel Mahmud <vxlrubel@gmail.com>
2026-01-30 00:01:18 +0600

    add project view file

--- src/views/project/ShowView.vue
+++ src/views/project/ShowView.vue
@@ -1,11 +1,16 @@
 <script setup>
+import LinkBack from '@/components/LinkBack.vue'
 import { onMounted } from 'vue'
 import { storeToRefs } from 'pinia'
 import { useRoute } from 'vue-router'
 import { useProjectStore } from '@/stores/project'
+import IconPlus from '@/components/icons/IconPlus.vue'
+import DatePicker from 'primevue/datepicker'
+
+import Editor from 'primevue/editor'
 
 const projectStore = useProjectStore()
-const { project } = storeToRefs(projectStore)
+const { loading, project } = storeToRefs(projectStore)
 const route = useRoute()
 
 onMounted(async () => {
@@ -15,6 +20,24 @@ onMounted(async () => {
 
 <template>
   <div>
-    <pre>{{ project }}</pre>
+    <div class="py-1 border-b border-gray-600 mb-3 flex items-center gap-2">
+      <LinkBack />
+      <button type="button" class="button">Create</button>
+    </div>
+
+    <template v-if="loading">
+      <div>Loading...</div>
+    </template>
+
+    <template v-else>
+      <div>
+        <p>{{ project?.name ?? 'Not defined' }}</p>
+        <p class="text-sm text-gray-300">{{ project?.description ?? 'No description' }}</p>
+      </div>
+
+      <div class="mt-3">
+        <div class="grid grid-cols-1 lg:grid-cols-2 gap-10">
+          <div class="grid grid-cols-1 text-sm border-t border-l border-r border-gray-600">
+            
   </div>
 </template>
