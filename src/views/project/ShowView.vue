af194782e579290dabf30dc38b9acfecf1d9be96
Rubel Mahmud <vxlrubel@gmail.com>
2026-01-30 23:50:33 +0600

    update view of project

--- src/views/project/ShowView.vue
+++ src/views/project/ShowView.vue
@@ -3,18 +3,27 @@ import LinkBack from '@/components/LinkBack.vue'
 import { onMounted } from 'vue'
 import { storeToRefs } from 'pinia'
 import { useRoute } from 'vue-router'
+
+import { useTaskStore } from '@/stores/task'
 import { useProjectStore } from '@/stores/project'
+import { useUserStore } from '@/stores/user'
+
 import IconPlus from '@/components/icons/IconPlus.vue'
 import DatePicker from 'primevue/datepicker'
-
 import Editor from 'primevue/editor'
+const userStore = useUserStore()
 
 const projectStore = useProjectStore()
 const { loading, project } = storeToRefs(projectStore)
 const route = useRoute()
+const taskStore = useTaskStore()
+const { projectTasks } = storeToRefs(taskStore)
 
 onMounted(async () => {
   projectStore.fetchProjectById(route.params.id)
+})
+onMounted(async () => {
+  taskStore.fetchTaskByProjectId(route.params.id)
 })
 </script>
 
@@ -24,7 +33,7 @@ onMounted(async () => {
       <LinkBack />
       <button type="button" class="button">Create</button>
     </div>
-
+    <pre>{{ projectTasks }}</pre>
     <template v-if="loading">
       <div>Loading...</div>
     </template>
@@ -38,24 +47,26 @@ onMounted(async () => {
       <div class="mt-3">
         <div class="grid grid-cols-1 lg:grid-cols-2 gap-10">
           <div class="grid grid-cols-1 text-sm border-t border-l border-r border-gray-600">
-            <div class="task-list" v-for="n in 10">
-              <div class="flex-1 p-1">task name</div>
+            <div class="task-list" v-for="task in projectTasks" :key="task.id">
+              <div class="flex-1 p-1">{{ task?.title }}</div>
               <div
                 class="w-60 p-1 flex items-center gap-1 border-l border-gray-600 text-[13px] relative z-10"
               >
                 <IconPlus />
                 Asign
+                {{ userStore.getUserNameById(task.assigned_to) }}
               </div>
               <div
                 class="w-40 p-1 flex items-center gap-1 border-l border-gray-600 custom-date-picker"
               >
-                <DatePicker />
+                <span>{{ task.due_date }}</span>
+                <!-- <DatePicker v-model="task.due_date" /> -->
               </div>
             </div>
           </div>
 
           <div>
-            <Editor v-model="value" editorStyle="height: 320px" />
+            <!-- <Editor v-model="value" editorStyle="height: 320px" /> -->
           </div>
         </div>
       </div>
